f0518e83e8f396b49f62b295b79bbf404b58dbf0
shrnzz <114313101+shrnzz@users.noreply.github.com>
2025-05-02 19:53:01 -0400

    day code fix

--- calendar.c
+++ calendar.c
@@ -54,15 +54,23 @@ int dayCode = GetDayCode(year);
 
     //print the days of the month 
         for (int day = 1; day <= monthLength[month]; day++){
+           if (HasEvent(month, day, year)){
+            printf("\033[1;32m%4d\033[0m", day);
+           }
+           else {
             printf("%4d", day);
+           }
 
-            if ((day + dayCode) % 7 != 0){
-            printf("\n"); // new line for the next week 
-            }
+           if ((day + dayCode) % 7 == 0){
+            printf("\n");
+           }
         }
 
-        printf("\n");
-        dayCode = (dayCode + monthLength[month]) % 7; // update daycode for the next month 
+        if ((monthLength[month] + dayCode) % 7 != 0){
+            printf("\n");
+        }
+
+        dayCode = (dayCode + monthLength[month]) % 7;
     }
 }
 
